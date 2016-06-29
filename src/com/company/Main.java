0ad41fd98c78e16ff0af43f72ba685400a6fad55
Help Desk <Help Desk>
2016-06-29 13:32:51 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+Scanner sc = new Scanner(System.in, "ISO-8859-2");
+        System.out.println("Zadejte èíslo k umocnìní.");
+        //int a = Integer.parseInt(sc.nextLine());
+        int a = sc.nextInt();
+        int b = a * a;
+        System.out.println("Èíslo je " + b);
+
+    }
+}

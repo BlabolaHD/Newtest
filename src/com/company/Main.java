74ff2cf21832bb440e25ed8ffc735eb3e8472dd6
helpd <jsemtu@jsemtu.com>
2016-07-04 16:03:56 +0200

    uprava

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
         // write your code here
         Scanner sc = new Scanner(System.in, "ISO-8859-2");
         System.out.println("Zadejte èíslo násobilky.");
-        int j = sc.nextInt();
 
-        for (;j <= 10; j++)
+        //for (int j = 1;j <= 10; j++)
+        int j = sc.nextInt();
         {
             for (int i = 1; i <= 10; i++)
                 System.out.printf("%d ", i * j);
